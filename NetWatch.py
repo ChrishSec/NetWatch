75cc1de03fcf3d782164ce68b33f01448d8ef882
ChrishSec <67695405+ChrishSec@users.noreply.github.com>
2024-01-25 08:33:22 -0800

    Add files via upload

---
+++ NetWatch.py
@@ -0,0 +1,92 @@
+"""
+
+__author__ = "ChrishSec"
+__copyright__ = "Copyright (C) 2024 ChrishSec"
+__license__ = "GNU General Public License v3.0"
+__version__ = "1.0.0"
+
+Website: https://ChrishSec.com
+GitHub: https://github.com/ChrishSec
+Twitter: https://twitter.com/ChrishSec
+
+"""
+
+import re
+import sys
+import time
+import pytz
+import random
+import argparse
+import subprocess
+from datetime import datetime
+
+log_file = 'access.log'
+previous_connections = set()
+
+def colorize(text, color_code):
+    return f"\033[{color_code}m{text}\033[0m"
+
+def random_color_code():
+    return random.randint(31, 37)
+
+def log_error(error_msg):
+    error_msg_colored = colorize(error_msg, '91')
+    print(f"\n ERROR: {error_msg_colored}\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Monitor network connections and log new connections.')
+    parser.add_argument('-timezone', help='Specify the timezone (e.g., "SG")', required=True)
+    args = parser.parse_args()
+
+    country_name = args.timezone
+
+    try:
+        while True:
+            result = subprocess.run(['netstat', '-an'], stdout=subprocess.PIPE, text=True, check=True)
+            connections = re.findall(r'(\w+)\s+(\d+)\s+(\d+)\s+(\d+\.\d+\.\d+\.\d+:\d+)\s+(\d+\.\d+\.\d+\.\d+:\d+)\s+(\w+)', result.stdout)
+
+            with open(log_file, 'a') as f:
+                for proto, recv_q, send_q, local_address, foreign_address, state in connections:
+                    if local_address not in previous_connections and foreign_address not in previous_connections:
+                        time_zone = pytz.country_timezones.get(country_name, [])[0]
+                        if time_zone:
+                            utc_now = datetime.utcnow()
+                            local_time = utc_now.replace(tzinfo=pytz.utc).astimezone(pytz.timezone(time_zone))
+                            formatted_time = local_time.strftime("\033[91m%b %d, %Y - %I:%M:%S %p\033[0m")
+
+                            proto_colored = colorize(proto, '32')
+                            recv_q_colored = colorize(recv_q, '32')
+                            send_q_colored = colorize(send_q, '32')
+                            local_address_colored = colorize(local_address, '32')
+                            foreign_address_colored = colorize(foreign_address, '32')
+
+                            if state == 'State:':
+                                state_colored = colorize(state, '32')
+                            elif state == 'ESTABLISHED':
+                                state_colored = colorize(state, '31')
+                            else:
+                                state_colored = colorize(state, '33')
+
+                            developed_by_colored = colorize("DEVELOPED BY >> ChrishSec.com", random_color_code())
+                            plus_sign_colored = colorize("[+]", '31')
+
+                            log_data = f"\n {developed_by_colored} \n\n {plus_sign_colored} New Connection Established ↓\n\n \033[32mConnection Time: {formatted_time}\033[0m\n \033[32mProto: {proto_colored}\033[0m\n \033[32mRecv-Q: {recv_q_colored}\033[0m\n \033[32mSend-Q: {send_q_colored}\033[0m\n \033[32mLocal Address: {local_address_colored}\033[0m\n \033[32mForeign Address: {foreign_address_colored}\033[0m\n \033[32mState: {state_colored}\033[0m"
+
+                            print(log_data)
+                            print("\n------------")
+
+                            f.write(log_data + '\n')
+
+                            previous_connections.add(local_address)
+                            previous_connections.add(foreign_address)
+
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print("\n Exiting.")
+        sys.exit(0)
+    except subprocess.CalledProcessError as e:
+        log_error(f" [!] Failed to run NetWatch.py Error: {e}")
+    except Exception as e:
+        log_error(f" [!] An error occurred: {e}")
+
---
+++ README.md
@@ -0,0 +1,46 @@
+## NetWatch
+
+`NetWatch` is a Python script designed to monitors network connections and logs new connections. It displays information about the established connections, such as connection time, protocol, receive queue, send queue, local and foreign addresses, and connection state.
+
+Note: This script utilizes the netstat command to gather network connection information. Please ensure that the netstat command is available on your system.
+
+### Requirements
+- Python 3.x
+- Unix-like operating system (tested on Ubuntu 20.04)
+
+### Usage
+
+1. Clone the repository:
+
+```git clone https://github.com/ChrishSec/NetWatch.git```
+
+2. Install the required dependencies:
+
+```pip3 install -r requirements.txt```
+
+3. Run the script:
+
+```python3 NetWatch.py -timezone <YOUR_TIMEZONE>```
+
+Replace ```<YOUR_TIMEZONE>``` with the desired timezone (e.g., "SG").
+
+The script will continuously monitor and display new network connections.
+
+### Screenshots
+
+Coming Soon
+
+![Screenshot 1](screenshots/screenshot_1.png)
+![Screenshot 2](screenshots/screenshot_2.png)
+
+## Disclaimer
+
+This script is intended for educational and research purposes only. Use it at your own risk. The author is not responsible for any damage caused by the use or misuse of this script.
+
+## License
+
+This script is released under the GNU General Public License v3.0. Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
+
+## Author
+
+This script was developed by [ChrishSec](https://github.com/ChrishSec). Feel free to fork, modify, and distribute it. If you have any questions or suggestions, please reach out to the author on [Telegram](https://t.me/ChrishSec).
